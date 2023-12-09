ac340077cddf7abe4cb4725673c9d3e9d49f42ae
bruna270498 <brunaalmeida075@gmail.com>
2023-12-09 14:10:05 -0300

    dando pull

--- projeto/db/api.js
+++ projeto/db/api.js
@@ -13,29 +13,6 @@ const showVacinasCount = async () => {
     const result = await pool.query('SELECT count(*) from VACINA');
     console.log(result.rows);
 }
-
-const criarPaciente = async (id_paciente, nome, data_nascimento) => {
-    const result = await pool.query('INSERT INTO PACIENTE (ID_PACIENTE, NOME, DATA_NASCIMENTO) VALUES ($1, $2, $3)', [id_paciente, nome, data_nascimento]);
-
-    return result;
-}
-
-const mostrarPacientes = async (id) => {
-    // const result = await pool.query(`SELECT NOME, DATA_NASCIMENTO FROM PACIENTE WHERE NOME = $1`, [`%${nome}%`]);
-    const result = await pool.query(`SELECT NOME, DATA_NASCIMENTO FROM PACIENTE WHERE ID_PACIENTE = $1`, [id]);
-    return result.rows;
-}
-
-const atualizarPaciente = async (nome, dataNascimento, id) => {
-    const result = await pool.query('UPDATE PACIENTE SET NOME = $1, DATA_NASCIMENTO = $2 WHERE ID_PACIENTE = $3', [nome, dataNascimento, id]);
-
-    console.log(result.rows);
-}
-
-// console.log(pool.options.user);
-// console.log(pool.options.database);
-// showVacinasCount();
-// mostrarPacientes();
-
-
-module.exports = { showVacinasCount, criarPaciente, atualizarPaciente, mostrarPacientes };
+console.log(pool.options.user);
+console.log(pool.options.database);
+showVacinasCount();
